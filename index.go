744f85e871bbd6eaf8decd84f48cc13c721d0603
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-24 13:23:03 +0530

    Update index.go

--- index.go
+++ index.go
@@ -1,3 +1,4 @@
+/*
 package main         // package declaration
 
 import "fmt"         // importing the required packages
@@ -7,10 +8,30 @@ func main() {        // starts execution of program
 
 	fmt.Printf("%T", x)
 }
-/* we can declare variables in 3 ways:
+
+ we can declare variables in 3 ways:
 var x int = 15
    OR
 var x = 15
    OR
 x := 15
+
+
+// Mixed variable declaration
+package main
+
+import "fmt"
+
+func main(){
+	var a,b,c= 12, 15.6, "foo"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("Type of a is %T\n",a)
+	fmt.Printf("Type of b is %T\n",b)
+	fmt.Printf("Type of c is %T\n",c)
+}
 */
+
+package main
