70fcfc8263aac02d652e907dc6218d4bd805ef1e
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-24 12:00:20 +0530

    Update index.go

--- index.go
+++ index.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Printf("Welcome to GO language")
-}
+func main() {
+	var x int= 12
 
+	fmt.Printf("%T", x)
+}
+/* we can declare variables in 3 ways:
+var x int = 15
+   OR
+var x = 15
+   OR
+x := 15
+*/
