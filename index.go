a6976823810a28d355d3abb85a29b8697bdf1445
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-28 10:58:52 +0530

    Update index.go

--- index.go
+++ index.go
@@ -212,6 +212,13 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("welcome")
+func main() {
+   /* create a slice */
+   numbers := []int{0,1,2,3,4,5,6,7,8} 
+   
+   /* print the numbers */
+   for i:= range numbers {
+      fmt.Println("Slice item",i,"is",numbers[i])
+   }
+   
 }
