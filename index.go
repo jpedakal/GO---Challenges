2d78af892f6abd2007c4f131d71ce3270a5da30d
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-28 21:00:34 +0530

    Update index.go

--- index.go
+++ index.go
@@ -208,17 +208,12 @@ func printBook(book *Books) {
 }
 */
 
+// map in GO
+
 package main
 
 import "fmt"
 
 func main() {
-   /* create a slice */
-   numbers := []int{0,1,2,3,4,5,6,7,8} 
-   
-   /* print the numbers */
-   for i:= range numbers {
-      fmt.Println("Slice item",i,"is",numbers[i])
-   }
-   
+	fmt.Println("welcome")
 }
