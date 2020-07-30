3a0fd22bf0f7cb12f1ff3aba2b2527cfb6081beb
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-30 10:31:50 +0530

    Update app.go

--- app.go
+++ app.go
@@ -7,13 +7,10 @@ import (
 )
 
 // RouteHandler export
-func RouteHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<h1>Welcome To GO</h1>")
-}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+        fmt.Fprintln(w, "<h1>Welcome To GO</h1>")
 	})
 	http.ListenAndServe(":3000", nil)
 }
