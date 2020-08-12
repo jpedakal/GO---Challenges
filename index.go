ebb6b81d6e847200da05cec3f660ed74144bd23b
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-12 21:23:42 +0530

    Update index.go

--- index.go
+++ index.go
@@ -435,7 +435,7 @@ import (
 
 func main() {
 	conn, _ := net.Dial("tcp", "golang.org:80")
-	//fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	// fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 	status, _ := bufio.NewReader(conn).ReadString('\n')
 	fmt.Println(status)
 }
